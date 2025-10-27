608eeab55075d027633f7d20a267ff3343446ddd
LAPTOP-RBNQ6FGR\minhp <minhphunguyenhoang0603@gmail.com>
2025-10-27 09:43:04 +0700

    Init LAB3

---
+++ STM32/Core/Src/global.c
@@ -0,0 +1,20 @@
+/*
+ * global.c
+ *
+ *  Created on: Oct 11, 2025
+ *      Author: minhp
+ */
+
+#include "global.h"
+
+int status = INIT;
+int counter1 = 0;
+int counter2 = 0;
+
+int time_red = 5;
+int time_amber = 2;
+int time_green = 3;
+
+int temp_red = 5;
+int temp_amber = 2;
+int temp_green = 3;
